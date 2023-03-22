f8ef72a6bdc6c80574fbdab8e26e6b87e8ecc3b4
KENGI ORTEGA <kortega90@hotmail.com>
2023-03-22 11:37:46 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+
+import "./styles.css";
+
+
+export default function Header() {
+  return (
+    <header>
+      <div className="header-content-container container">
+        <h1 className="site-name">DSFilter</h1>
+        <p>6 produto(s)</p>
+      </div>
+    </header>
+  );
+}
